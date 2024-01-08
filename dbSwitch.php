5ad4c5093a555b06bf9794c82ae5ce04b121dad7
Aayush Adhikari <adhikariaayush37@gmail.com>
2024-01-09 02:05:34 +0545

    initiation

---
+++ dbSwitch.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ *  @package dbSwitchPlugin
+ */
+
+/*
+Plugin Name: dbSwitchPlugin
+Plugin URI: github.com/aayush518/dbSwitchPlugin
+Description: This plugin is used to switch color of the website from dark to light and vice versa.
+Version: 1.0.0
+Author: Aayush Adhikari
+Author URI: github.com/Aayush518
+License: GPLv2 or later
+Text Domain: dbSwitchPlugin
+*/
+
+/*
+Copyright (C) 2024 Aayush
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+*/
